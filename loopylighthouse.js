56ae940268e1b3f56a4091f62ff4005258aca5e6
Akhila499 <akhilathota499@gmail.com>
2021-06-29 09:58:14 -0400

    hello.js

---
+++ loopylighthouse.js
@@ -0,0 +1,12 @@
+for (var x = 100 ;  x <= 200 ; x++){
+  if ( x % 12 === 0){
+    console.log('Loopy Lighthouse');
+  }else if( x % 4 === 0){
+    console.log('Lighthouse');
+  }else if (x % 3 === 0){
+    console.log('Loopy ');
+  }else{
+    console.log(x);
+  }
+ 
+}
